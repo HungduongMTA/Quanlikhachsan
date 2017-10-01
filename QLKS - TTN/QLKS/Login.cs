cb0806fb6294c3d2785d44331be702e0855b4f2f
HungduongMTA <31845257+HungduongMTA@users.noreply.github.com>
2017-10-01 09:03:18 +0700

    Login

--- QLKS - TTN/QLKS/Login.cs
+++ QLKS - TTN/QLKS/Login.cs
@@ -16,26 +16,39 @@ namespace QLKS
         {
             InitializeComponent();
         }
-    string cnn = @"Data Source=desktop-loq2vp6\sqlexpress;Initial Catalog=QLKhachsan;Integrated Security=True";
-       
+        string cnn = @"Data Source=desktop-loq2vp6\sqlexpress;Initial Catalog=QLKhachsan;Integrated Security=True";
+
         DataContextDataContext dt = new DataContextDataContext();
         DataTable dt2 = new DataTable();
-        
+
         private void Login_Load(object sender, EventArgs e)
         {
 
         }
-       
+
 
         private void bntdangnhap_Click(object sender, EventArgs e)
         {
-            
-            
+            SqlConnection conn = new SqlConnection(cnn);
+            SqlDataAdapter da = new SqlDataAdapter("Select count(*) from TAIKHOAN where TENTAIKHOAN='" + txttaikhoan.Text + "' and MATKHAU='" + txtmatkhau.Text + "'", conn);
+            DataTable dtt = new DataTable();
+            da.Fill(dtt);
+            if (dtt.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+                Form1 f = new Form1(txttaikhoan.Text);
+                f.Show();
+            }
+            else
+            {
+                MessageBox.Show("Xin Kiểm tra lại thông tin đăng nhập");
+            }
+
         }
 
         private void bntthoat_Click(object sender, EventArgs e)
         {
-            
+            this.Close();
         }
     }
 }
