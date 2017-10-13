6b336f246b69e5510c387b8a7a10d40fbc816fbd
NhuNam50 <32055881+NhuNam50@users.noreply.github.com>
2017-10-14 00:15:16 +0700

    dich vu su dung

--- QLKS - TTN/QLKS/Dichvusudung.cs
+++ QLKS - TTN/QLKS/Dichvusudung.cs
@@ -22,48 +22,90 @@ namespace QLKS
         private void ketnoicsdl()
         {
 
-           
+            datadvsd.DataSource = dt.Select_Dichvusudung();
         }
 
         private void datadvsd_CellClick(object sender, DataGridViewCellEventArgs e)
         {
 
-           
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                txtcmnd.Text = Convert.ToString(datadvsd.CurrentRow.Cells["Socmnd"].Value);
+                txtmadv.Text = Convert.ToString(datadvsd.CurrentRow.Cells["Madv"].Value);
+                txtsoluong.Text = Convert.ToString(datadvsd.CurrentRow.Cells["Soluong"].Value);
+                txtthanhtoan.Text = Convert.ToString(datadvsd.CurrentRow.Cells["Thanhtoan"].Value);
+
+            }
         }
 
         private void datadvsd_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
         {
-            
+            datadvsd.Rows[e.RowIndex].Cells["STT"].Value = e.RowIndex + 1;
         }
 
         private void Dichvusudung_Load(object sender, EventArgs e)
         {
-            
+            datadvsd.DataSource = dt.UPDATE_Dichvusudung();
+            conn = new SqlConnection(cnn);
+            conn.Open();
+            ketnoicsdl();
         }
 
         private void bntthem_Click(object sender, EventArgs e)
         {
-           
+            Themdichvusudung t = new Themdichvusudung();
+            t.Show();
         }
         int ad;
         private void bntsua_Click(object sender, EventArgs e)
         {
-           
+            txtcmnd.Enabled = true;
+            txtcmnd.Focus();
+            ad = 1;
+            bntthem.Enabled = false;
+            bntxoa.Enabled = false;
+            bntsua.Enabled = false;
         }
 
         private void bntluu_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                if (ad == 1)
+                {
+                    dt.Update_DVSD(txtcmnd.Text, txtmadv.Text, Int32.Parse(txtsoluong.Text), float.Parse(txtthanhtoan.Text));
+
+                    ad = 0;
+                    bntthem.Enabled = true;
+                    bntxoa.Enabled = true;
+                    bntsua.Enabled = true;
+                }
+                Dichvusudung_Load(sender, e);
+            }
+            catch
+            {
+                MessageBox.Show("Xin kiểm tra lại thông tin !");
+            }
         }
 
         private void bntxoa_Click(object sender, EventArgs e)
         {
-            
+            if (MessageBox.Show("Bạn có muốn xóa không?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+
+                dt.Xoa_DVSD(txtcmnd.Text);
+                txtcmnd.Text = "";
+                txtmadv.Text = "";
+                txtsoluong.Text = "";
+                txtthanhtoan.Text = "";
+
+                Dichvusudung_Load(sender, e);
+            }
         }
 
         private void bntcapnhat_Click(object sender, EventArgs e)
         {
-           
+            Dichvusudung_Load(sender, e);
         }
     }
 }
