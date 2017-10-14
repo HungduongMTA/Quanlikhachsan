08afdc2e0ee772d0c178bd57fa0cda69bb927159
HungCong <31845368+HungCong@users.noreply.github.com>
2017-10-14 16:18:18 +0700

    Form Thanh toán

--- QLKS - TTN/QLKS/ThanhToan.cs
+++ QLKS - TTN/QLKS/ThanhToan.cs
@@ -21,19 +21,36 @@ namespace QLKS
         DataContextDataContext dt = new DataContextDataContext();
         private void ketnoicsdl()
         {
-           
-            
-            
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM KHACHHANG", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            datakhachhang.DataSource = dt;
+
         }
 
         private void ThanhToan_Load(object sender, EventArgs e)
         {
-           
+            datakhachhang.DataSource = dt.Update_Thanhtoan();
+            conn = new SqlConnection(cnn);
+            conn.Open();
+            ketnoicsdl();
         }
 
         private void datakhachhang_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                txthoten.Text = Convert.ToString(datakhachhang.CurrentRow.Cells["HoTen"].Value);
+                txtcmnd.Text = Convert.ToString(datakhachhang.CurrentRow.Cells["Socmnd"].Value);
+                txtsdt.Text = Convert.ToString(datakhachhang.CurrentRow.Cells["SDT"].Value);
+                txtdiachi.Text = Convert.ToString(datakhachhang.CurrentRow.Cells["Diachi"].Value);
+                txtngayden.Text = Convert.ToString(datakhachhang.CurrentRow.Cells["Ngayden"].Value);
+                txtngaytra.Text = Convert.ToString(datakhachhang.CurrentRow.Cells["Ngaytra"].Value);
+                txtmaphong.Text = Convert.ToString(datakhachhang.CurrentRow.Cells["MaPhong"].Value);
+                txttongtien.Text = Convert.ToString(datakhachhang.CurrentRow.Cells["Tienthanhtoan"].Value);
+                datathanhtoan.DataSource = dt.Select_ThanhtoanDVSD(txtcmnd.Text);
+            }
         }
     }
 }
