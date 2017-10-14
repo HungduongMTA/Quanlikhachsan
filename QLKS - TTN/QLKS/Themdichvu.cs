1ad6bb505e4c771f5e15c8e69eb87d903b2d5062
nguyenthang92 <gadanhxe.vp@gmail.com>
2017-10-14 09:11:42 +0700

    Form ThemDichVu

--- QLKS - TTN/QLKS/Themdichvu.cs
+++ QLKS - TTN/QLKS/Themdichvu.cs
@@ -21,17 +21,20 @@ namespace QLKS
         DataContextDataContext dt = new DataContextDataContext();
         private void Themdichvu_Load(object sender, EventArgs e)
         {
-           
+            conn = new SqlConnection(cnn);
+            conn.Open();
         }
 
         private void bntthem_Click(object sender, EventArgs e)
         {
-           
+            dt.Them_Dichvu(txtmadv.Text, txttendv.Text, float.Parse(txtgiadv.Text));
+            MessageBox.Show("Thêm mới thành công!");
+            Themdichvu_Load(sender, e);
         }
 
         private void bnthoat_Click(object sender, EventArgs e)
         {
-           
+            this.Close();
         }
     }
 }
